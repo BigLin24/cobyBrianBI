59d254f5154591a63821ef2097d02cb5937bc43a
Daniel K <daniel.kmiotek@gmx.de>
2018-07-12 18:52:06 +0200

    Anpassung NaN

--- data_Preperation-v2.py
+++ data_Preperation-v2.py
@@ -11,30 +11,40 @@ import matplotlib.pyplot as plt
 data = pd.read_csv('data.csv')
 
 
-''' Loescht Eintraege bei welchen kein 
-    "shot_made_flag" vorhanden ist und speicher in das Dataframe
-    "dropped" ab'''
-dropped = data.dropna(subset=['shot_made_flag'])
 
-#dropped.to_csv('testfileDropped.csv')
 
 
 ''' Fasst "minutes_remaining" und "secondas_remaining" zusammen
     in der "Spalte" "remaining_time" im Dataframe dropped'''
-dropped['remaining_time'] = dropped['minutes_remaining'] * 60 + dropped['seconds_remaining']
+data['remaining_time'] = data['minutes_remaining'] * 60 + data['seconds_remaining']
 
 
 ''' Berechnet die Spalte "dist" im Dataframe "dropped" durch:
     (WURZEL von loc_x) HOCH 2 + (loc_y)HOCH 2) '''
     
-dropped['dist'] = np.sqrt(dropped['loc_x']**2 + dropped['loc_y']**2)
+data['dist'] = np.sqrt(data['loc_x']**2 + data['loc_y']**2)
 
 ''' Speicher das Dataframe "dropped" in die datei  "cleanedFile.csv"'''
+data.to_csv('cleanedFileWithNaN.csv')
+
+
+
+''' Loescht Eintraege bei welchen kein 
+    "shot_made_flag" vorhanden ist und speicher in das Dataframe
+    "dropped" ab'''
+dropped = data.dropna(subset=['shot_made_flag'])
+
+''' Speicher das Dataframe "Data" in die datei  "cleanedFile.csv"'''
 dropped.to_csv('cleanedFile.csv')
+
+
 
 
 ''' Die Funktion gibt das Dataframe zurueck.
     Relevant in anderen Dokumente, um den Dataframe zu erhalten'''
     
 def getDataFrameKobeBryant():
+    return dropped
+
+def getDataFrameKobeBryantWithNaN():
     return dropped
