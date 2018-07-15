c05afcf7ada15407ca7518dd660266fcaf72faa5
Daniel K <daniel.kmiotek@gmx.de>
2018-07-15 22:50:51 +0200

    Naives Byes

---
+++ NaiveBayes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 15 22:48:17 2018
+
+@author: biglin
+"""
+
+
+import ClassifierLoader
+from sklearn import naive_bayes
+
+obj = naive_bayes.GaussianNB()
+obj1 = obj
+
+def fitNaiveBayes(x_train, y_train):
+    obj = fit(obj1, x_train, y_train )
+
+def scoreNaiveBayes(x_test, y_test):
+    score(obj, x_test, y_test)
+    
+def predictNaiveBayes(x_predict):
+    yTarget = predictNaNx(obj, x_predict)
+    return yTarget
+
+def plotNaiveBayes(x_plot, y_plot):
+    plot(obj, x_plot, y_plot, "NaiveBayes")
